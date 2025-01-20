01ed783f845627e5a0061ae3090b084f5f2669d0
TiborMaron <tibormaron@gmail.com>
2025-01-20 02:12:34 +0100

    Added Draw Card as home page with basic functionality

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class _HomePageState extends State<HomePage> {
   int selectedIndex = 0;
 
   final List<Widget> pages = [
-    Placeholder(),
+    DrawCard(),
     Placeholder(),
     Placeholder(),
     const TestPage(),
@@ -57,7 +57,7 @@ class _HomePageState extends State<HomePage> {
                   destinations: const [
                     NavigationRailDestination(
                       icon: Icon(Icons.home),
-                      label: Text('Home'),
+                      label: Text('Draw Card'),
                     ),
                     NavigationRailDestination(
                       icon: Icon(Icons.history),
@@ -133,6 +133,55 @@ class _TestPageState extends State<TestPage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class DrawCard extends StatefulWidget {
+  const DrawCard({super.key});
+
+  @override
+  DrawCardState createState() => DrawCardState();
+}
+
+class DrawCardState extends State<DrawCard> {
+  bool _showImage = false;
+
+  void _toggleImage() {
+    setState(() {
+      _showImage = !_showImage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(0, 0, 0, 150),
+      body: Stack(
+        children: [
+          Center(
+            child: ElevatedButton.icon(
+              onPressed: _toggleImage,
+              label: const Text('Show Card'),
+            ),
+          ),
+          if (_showImage)
+            GestureDetector(
+              onTap: _toggleImage,
+              child: Container(
+                alignment: Alignment.center,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: Image.asset(
+                    'assets/cards/the_tower.webp',
+                    height: MediaQuery.of(context).size.height * 0.8,
+                    fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
