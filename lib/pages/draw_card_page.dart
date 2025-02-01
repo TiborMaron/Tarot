126fedf20d2417f364f99111fba1d6b8d4525f4d
TiborMaron <tibormaron@gmail.com>
2025-02-02 00:01:14 +0100

    Made the code more readable

--- lib/pages/draw_card_page.dart
+++ lib/pages/draw_card_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class DrawCard extends StatefulWidget {
   const DrawCard({super.key});
@@ -36,72 +37,82 @@ class DrawCardState extends State<DrawCard> with SingleTickerProviderStateMixin 
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Stack(
+      body: Column(
         children: [
-          Center(
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 7,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: AnimatedBuilder(
-                      animation: _controller,
-                      builder: (context, child) {
-                        final rotationValue = _controller.value * 3.14159; // PI for 180-degree rotation
-                        final isFlipped = rotationValue > 3.14159 / 2;
-                        return Transform(
-                          alignment: Alignment.center,
-                          transform: Matrix4.identity()
-                            ..setEntry(3, 2, 0.001)
-                            ..rotateY(rotationValue),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: isFlipped
-                                ? Image.asset(
-                                    'assets/cards/the_tower.webp',
-                                    fit: BoxFit.contain,
-                                  )
-                                : Image.asset(
-                                    'assets/cards/cover.webp',
-                                    fit: BoxFit.contain,
-                                  ),
-                          ),
-                        );
-                      },
-                    ),
+          Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+
+            // The Card
+            Expanded(
+            flex: 7,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  final rotationValue = _controller.value * math.pi;
+                  final isFlipped = rotationValue > math.pi / 2;
+                  return Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..rotateY(rotationValue),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: isFlipped
+                          ? Image.asset(
+                              'assets/cards/the_tower.webp',
+                              fit: BoxFit.contain,
+                            )
+                          : Image.asset(
+                              'assets/cards/cover.webp',
+                              fit: BoxFit.contain,
+                            ),
+                        ),
+                      );
+                    },
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintStyle: TextStyle(color: Colors.grey),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
-                        borderRadius: BorderRadius.circular(16.0)
-                      ),
-                      hintText: 'Write your question here!',
-                    ),
-                    textAlignVertical: TextAlignVertical.top,
+              ),
+
+            // TextField
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(16.0)
                   ),
+                  hintText: 'Write your question here!',
                 ),
-                SizedBox(height: 16),
-                ElevatedButton.icon(
-                  onPressed: _toggleImage,
-                  icon: const Icon(Icons.visibility, color: Colors.black),
-                  label: const Text(
-                    'Show Card',
-                    style: TextStyle(color: Colors.black),),
-                ),
-                SizedBox(height: 20),
-              ],
+                textAlignVertical: TextAlignVertical.top,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // Button
+            ElevatedButton.icon(
+              onPressed: _toggleImage,
+              icon: const Icon(Icons.visibility, color: Colors.black),
+              label: const Text(
+                'Show Card',
+              style: TextStyle(color: Colors.black),),
+            ),
+            
+            const SizedBox(height: 10),
+
+            ],
             ),
           ),
-        ],
-      ),
+        ]
+      )
     );
   }
 }
