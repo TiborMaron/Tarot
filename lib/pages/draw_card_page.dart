8fa14de8c9f824da39cefed0136f1d2435525575
TiborMaron <tibormaron@gmail.com>
2025-02-02 15:57:26 +0100

    Added Nine of Wands

--- lib/pages/draw_card_page.dart
+++ lib/pages/draw_card_page.dart
@@ -28,6 +28,7 @@ class DrawCardState extends State<DrawCard> with SingleTickerProviderStateMixin 
     Card(name: 'The Emperor', assetPath: 'assets/cards/the_emperor.webp'),
     Card(name: 'The Tower', assetPath: 'assets/cards/the_tower.webp'),
     Card(name: 'The Justice', assetPath: 'assets/cards/the_justice.webp'),
+    Card(name: 'Nine of Wands', assetPath: 'assets/cards/nine_of_wands.webp'),
   ];
 
   void _toggleImage() {
