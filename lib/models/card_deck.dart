97b2cb7242a12cca2c018596add95700e9aad121
TiborMaron <tibormaron@gmail.com>
2025-04-27 11:11:08 +0200

    Hard coded Crowley's deck

--- lib/models/card_deck.dart
+++ lib/models/card_deck.dart
@@ -1,8 +1,95 @@
 import 'card.dart';
 
 final List<Card> cardDeck = [
-  Card(name: 'The Emperor', assetPath: 'assets/cards/the_emperor.webp'),
-  Card(name: 'The Tower', assetPath: 'assets/cards/the_tower.webp'),
-  Card(name: 'The Justice', assetPath: 'assets/cards/the_justice.webp'),
-  Card(name: 'Nine of Wands', assetPath: 'assets/cards/nine_of_wands.webp'),
+
+    // Cover
+    Card(name: "Cover", assetPath: "assets/cards/crowley/cover.png"),
+
+    // Wands
+    Card(name: "Ace of Wands", assetPath: "assets/cards/crowley/Thot-W-0A.png"),
+    Card(name: "Two of Wands", assetPath: "assets/cards/crowley/Thot-W-02.png"),
+    Card(name: "Three of Wands", assetPath: "assets/cards/crowley/Thot-W-03.png"),
+    Card(name: "Four of Wands", assetPath: "assets/cards/crowley/Thot-W-04.png"),
+    Card(name: "Five of Wands", assetPath: "assets/cards/crowley/Thot-W-05.png"),
+    Card(name: "Six of Wands", assetPath: "assets/cards/crowley/Thot-W-06.png"),
+    Card(name: "Seven of Wands", assetPath: "assets/cards/crowley/Thot-W-07.png"),
+    Card(name: "Eight of Wands", assetPath: "assets/cards/crowley/Thot-W-08.png"),
+    Card(name: "Nine of Wands", assetPath: "assets/cards/crowley/Thot-W-09.png"),
+    Card(name: "Ten of Wands", assetPath: "assets/cards/crowley/Thot-W-10.png"),
+    Card(name: "Knight of Wands", assetPath: "assets/cards/crowley/Thot-W-KN.png"),
+    Card(name: "Queen of Wands", assetPath: "assets/cards/crowley/Thot-W-QU.png"),
+    Card(name: "Prince of Wands", assetPath: "assets/cards/crowley/Thot-W-PN.png"),
+    Card(name: "Princess of Wands", assetPath: "assets/cards/crowley/Thot-W-PS.png"),
+
+    // Cups
+    Card(name: "Ace of Cups", assetPath: "assets/cards/crowley/Thot-C-0A.png"),
+    Card(name: "Two of Cups", assetPath: "assets/cards/crowley/Thot-C-02.png"),
+    Card(name: "Three of Cups", assetPath: "assets/cards/crowley/Thot-C-03.png"),
+    Card(name: "Four of Cups", assetPath: "assets/cards/crowley/Thot-C-04.png"),
+    Card(name: "Five of Cups", assetPath: "assets/cards/crowley/Thot-C-05.png"),
+    Card(name: "Six of Cups", assetPath: "assets/cards/crowley/Thot-C-06.png"),
+    Card(name: "Seven of Cups", assetPath: "assets/cards/crowley/Thot-C-07.png"),
+    Card(name: "Eight of Cups", assetPath: "assets/cards/crowley/Thot-C-08.png"),
+    Card(name: "Nine of Cups", assetPath: "assets/cards/crowley/Thot-C-09.png"),
+    Card(name: "Ten of Cups", assetPath: "assets/cards/crowley/Thot-C-10.png"),
+    Card(name: "Knight of Cups", assetPath: "assets/cards/crowley/Thot-C-KN.png"),
+    Card(name: "Queen of Cups", assetPath: "assets/cards/crowley/Thot-C-QU.png"),
+    Card(name: "Prince of Cups", assetPath: "assets/cards/crowley/Thot-C-PN.png"),
+    Card(name: "Princess of Cups", assetPath: "assets/cards/crowley/Thot-C-PS.png"),
+
+    // Swords
+    Card(name: "Ace of Swords", assetPath: "assets/cards/crowley/Thot-S-0A.png"),
+    Card(name: "Two of Swords", assetPath: "assets/cards/crowley/Thot-S-02.png"),
+    Card(name: "Three of Swords", assetPath: "assets/cards/crowley/Thot-S-03.png"),
+    Card(name: "Four of Swords", assetPath: "assets/cards/crowley/Thot-S-04.png"),
+    Card(name: "Five of Swords", assetPath: "assets/cards/crowley/Thot-S-05.png"),
+    Card(name: "Six of Swords", assetPath: "assets/cards/crowley/Thot-S-06.png"),
+    Card(name: "Seven of Swords", assetPath: "assets/cards/crowley/Thot-S-07.png"),
+    Card(name: "Eight of Swords", assetPath: "assets/cards/crowley/Thot-S-08.png"),
+    Card(name: "Nine of Swords", assetPath: "assets/cards/crowley/Thot-S-09.png"),
+    Card(name: "Ten of Swords", assetPath: "assets/cards/crowley/Thot-S-10.png"),
+    Card(name: "Knight of Swords", assetPath: "assets/cards/crowley/Thot-S-KN.png"),
+    Card(name: "Queen of Swords", assetPath: "assets/cards/crowley/Thot-S-QU.png"),
+    Card(name: "Prince of Swords", assetPath: "assets/cards/crowley/Thot-S-PN.png"),
+    Card(name: "Princess of Swords", assetPath: "assets/cards/crowley/Thot-S-PS.png"),
+
+    // Disks
+    Card(name: "Ace of Disks", assetPath: "assets/cards/crowley/Thot-D-0A.png"),
+    Card(name: "Two of Disks", assetPath: "assets/cards/crowley/Thot-D-02.png"),
+    Card(name: "Three of Disks", assetPath: "assets/cards/crowley/Thot-D-03.png"),
+    Card(name: "Four of Disks", assetPath: "assets/cards/crowley/Thot-D-04.png"),
+    Card(name: "Five of Disks", assetPath: "assets/cards/crowley/Thot-D-05.png"),
+    Card(name: "Six of Disks", assetPath: "assets/cards/crowley/Thot-D-06.png"),
+    Card(name: "Seven of Disks", assetPath: "assets/cards/crowley/Thot-D-07.png"),
+    Card(name: "Eight of Disks", assetPath: "assets/cards/crowley/Thot-D-08.png"),
+    Card(name: "Nine of Disks", assetPath: "assets/cards/crowley/Thot-D-09.png"),
+    Card(name: "Ten of Disks", assetPath: "assets/cards/crowley/Thot-D-10.png"),
+    Card(name: "Knight of Disks", assetPath: "assets/cards/crowley/Thot-D-KN.png"),
+    Card(name: "Queen of Disks", assetPath: "assets/cards/crowley/Thot-D-QU.png"),
+    Card(name: "Prince of Disks", assetPath: "assets/cards/crowley/Thot-D-PN.png"),
+    Card(name: "Princess of Disks", assetPath: "assets/cards/crowley/Thot-D-PS.png"),
+
+    // Major Arcana
+    Card(name: "The Fool", assetPath: "assets/cards/crowley/Thot-T-00.png"),
+    Card(name: "The Magus", assetPath: "assets/cards/crowley/Thot-T-01.png"),
+    Card(name: "The High Priestess", assetPath: "assets/cards/crowley/Thot-T-02.png"),
+    Card(name: "The Empress", assetPath: "assets/cards/crowley/Thot-T-03.png"),
+    Card(name: "The Emperor", assetPath: "assets/cards/crowley/Thot-T-04.png"),
+    Card(name: "The Hierophant", assetPath: "assets/cards/crowley/Thot-T-05.png"),
+    Card(name: "The Lovers", assetPath: "assets/cards/crowley/Thot-T-06.png"),
+    Card(name: "The Chariot", assetPath: "assets/cards/crowley/Thot-T-07.png"),
+    Card(name: "Adjustment", assetPath: "assets/cards/crowley/Thot-T-08.png"), // Justice
+    Card(name: "The Hermit", assetPath: "assets/cards/crowley/Thot-T-09.png"),
+    Card(name: "Fortune", assetPath: "assets/cards/crowley/Thot-T-10.png"), // Wheel of Fortune
+    Card(name: "Lust", assetPath: "assets/cards/crowley/Thot-T-11.png"), // Strength
+    Card(name: "The Hanged Man", assetPath: "assets/cards/crowley/Thot-T-12.png"),
+    Card(name: "Death", assetPath: "assets/cards/crowley/Thot-T-13.png"),
+    Card(name: "Art", assetPath: "assets/cards/crowley/Thot-T-14.png"), // Temperance
+    Card(name: "The Devil", assetPath: "assets/cards/crowley/Thot-T-15.png"),
+    Card(name: "The Tower", assetPath: "assets/cards/crowley/Thot-T-16.png"),
+    Card(name: "The Star", assetPath: "assets/cards/crowley/Thot-T-17.png"),
+    Card(name: "The Moon", assetPath: "assets/cards/crowley/Thot-T-18.png"),
+    Card(name: "The Sun", assetPath: "assets/cards/crowley/Thot-T-19.png"),
+    Card(name: "The Aeon", assetPath: "assets/cards/crowley/Thot-T-20.png"), // Judgment
+    Card(name: "The Universe", assetPath: "assets/cards/crowley/Thot-T-21.png"),
 ];
